da822c2bd54955cf4d50ac9f65ca54ee9a17e5b2
SicksBack <muhammed69421@gmail.com>
2024-06-13 22:13:06 +0100

    testing webhoooooooook

--- src/main/java/org/brutality/module/impl/Fight/VelocityModule.java
+++ src/main/java/org/brutality/module/impl/Fight/VelocityModule.java
@@ -3,5 +3,7 @@ package org.brutality.module.impl.Fight;
 
 import org.brutality.module.Module;
 import org.brutality.module.impl.render.ClickGuiModule;
+import org.brutality.module.impl.Fight.VelocityModule;
+
 
 public class VelocityModule {
