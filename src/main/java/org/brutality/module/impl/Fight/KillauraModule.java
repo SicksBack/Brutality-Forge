ba773ee42e624a6729d193cc875e762c04f8a961
SicksBack <muhammed69421@gmail.com>
2024-06-14 18:26:04 +0100

    testing webhoooooooook

--- src/main/java/org/brutality/module/impl/Fight/KillauraModule.java
+++ src/main/java/org/brutality/module/impl/Fight/KillauraModule.java
@@ -1,27 +1,9 @@
 package org.brutality.module.impl.Fight;
 
+
 import org.brutality.module.Module;
+import org.brutality.module.impl.render.ClickGuiModule;
 import org.brutality.module.Category;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
-import org.lwjgl.input.Keyboard;
+import org.brutality.ui.clickGui.ClickGui;
 
-public class KillauraModule extends Module {
-
-    public KillauraModule() {
-        super("Sprint", "Makes the player sprint (essentially toggle sprint)", Category.MOVEMENT);
-        // Set keybinding
-        this.setKey(Keyboard.KEY_O);
-    }
-
-    public void onDisable() {
-        mc.thePlayer.setSprinting(mc.gameSettings.keyBindSprint.isKeyDown());
-        super.onDisable();
-    }
-
-    @SubscribeEvent
-    public void onTick(TickEvent.ClientTickEvent e) {
-        mc.thePlayer.setSprinting(true);
-        if (mc.thePlayer.moveForward <= 0.0f) {
-            mc.thePlayer.setSprinting(false);
-        }
+public class KillauraModule {
--- src/main/java/org/brutality/module/impl/Fight/VelocityModule.java
+++ src/main/java/org/brutality/module/impl/Fight/VelocityModule.java
@@ -3,7 +3,6 @@ package org.brutality.module.impl.Fight;
 
 import org.brutality.module.Module;
 import org.brutality.module.impl.render.ClickGuiModule;
-import org.brutality.module.impl.Fight.VelocityModule;
 import org.brutality.module.Category;
 import org.brutality.ui.clickGui.ClickGui;
 
