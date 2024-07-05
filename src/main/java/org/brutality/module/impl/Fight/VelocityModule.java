83de54e642da51163d4c8edf3bcc06eeb3cc3ebf
SicksBack <139510265+SicksBack@users.noreply.github.com>
2024-07-05 23:05:32 +0100

    Delete src directory

--- src/main/java/org/brutality/module/impl/Fight/VelocityModule.java
+++
@@ -1,4 +0,0 @@
-package org.brutality.module.impl.Fight;
-
-
-public class VelocityModule {
--- src/main/java/org/brutality/utils/interfaces/SM.java
+++
@@ -1,8 +0,0 @@
-package org.brutality.utils.interfaces;
-
-import org.brutality.BrutalityClient;
-import org.brutality.settings.SettingsManager;
-
-public interface SM {
-    SettingsManager sm = BrutalityClient.INSTANCE.settingsManager;
-}
