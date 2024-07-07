7ed4df05a8fd23c4ad184ff66f28e359f15c20bc
SicksBack <139510265+SicksBack@users.noreply.github.com>
2024-07-07 16:05:44 +0100

    Delete build directory

--- build/sources/main/java/org/brutality/utils/render/GuiUtil.java
+++
@@ -1,7 +0,0 @@
-package org.brutality.utils.render;
-
-public class GuiUtil {
-    public static boolean isHovered(float mouseX, float mouseY, float x, float y, float width, float height) {
-        return mouseX >= x && mouseY >= y && mouseX < x + width && mouseY < y + height;
-    }
-}
